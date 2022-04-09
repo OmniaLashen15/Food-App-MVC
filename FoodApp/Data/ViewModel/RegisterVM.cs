3a459e52d4b8fe414d5138e10475f5403e5e187d
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-04-09 17:14:45 +0200

    update registervm

--- FoodApp/Data/ViewModel/RegisterVM.cs
+++ FoodApp/Data/ViewModel/RegisterVM.cs
@@ -15,14 +15,14 @@ namespace FoodApp.Data.ViewModel
         [Required]
         [StringLength(10, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
         [RegularExpression(@"^.*(?=.{8,})(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
-         ErrorMessage = "At least one speacial charchter and one Uppercase.")]
+         ErrorMessage = "At least one speacial charchter @,#,$,%,^,& and one Uppercase.")]
 
         [DataType(DataType.Password)]
 
         public string Password { get; set; }
 
         [Display(Name = "Confirm password")]
-        [Required(ErrorMessage = "At least one speacial charchter @,#,$,%,^,& and one Uppercase.")]
+        [Required(ErrorMessage = "Confirm password is required")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Passwords do not match")]
         public string ConfirmPassword { get; set; }
